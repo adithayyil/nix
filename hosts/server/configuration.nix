c6d4f11789862d3483a41d8034ff5784ffbbbc9f
adithayyil <adithayyil@proton.me>
2025-12-13 17:06:56 -0500

    refactor(server): just use nm and fmt

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -20,20 +20,7 @@
     ../../profiles/server
   ];
 
-  # Server-specific: Static IP configuration
-  # Note: Adjust interface name (eth0/ens18/etc.) based on your hardware
-  networking.networkmanager.enable = false;
-  networking.interfaces.eth0.ipv4.addresses = [
-    {
-      address = "10.0.0.76";
-      prefixLength = 24;
-    }
-  ];
-  networking.defaultGateway = "192.168.1.1";
-  networking.nameservers = [
-    "1.1.1.1"
-    "1.0.0.1"
-  ];
+  networking.networkmanager.enable = true;
 
   # System packages
   environment.systemPackages = with pkgs; [
@@ -42,7 +29,10 @@
   ];
 
   # Trust this machine's nix store for deployments
-  nix.settings.trusted-users = [ "adi" "root" ];
+  nix.settings.trusted-users = [
+    "adi"
+    "root"
+  ];
 
   # [!!DO NOT CHANGE!!] NixOS release version [!!DO NOT CHANGE!!]
   system.stateVersion = "25.05";
