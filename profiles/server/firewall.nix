e65486d4710f46b2dadb4892f4941f0f948d5215
adithayyil <adithayyil@proton.me>
2025-12-14 13:26:17 -0500

    feat(firewall): allow http ports

--- profiles/server/firewall.nix
+++ profiles/server/firewall.nix
@@ -9,11 +9,11 @@
   networking.firewall = {
     enable = true;
 
-    allowedTCPPorts = [ 22 ];
-
-    # Common service ports (add as needed):
-    # allowedTCPPorts = [ 80 443 ]; # HTTP/HTTPS
-    # allowedUDPPorts = [ 51820 ];  # WireGuard
+    allowedTCPPorts = [
+      22
+      80
+      443
+    ];
 
     logRefusedConnections = true;
   };
