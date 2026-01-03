1143dedb9632d96249ec6359a37b016134f77bf5
adithayyil <adithayyil@proton.me>
2026-01-02 20:05:53 -0500

    chore: more basic nvim conf

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -14,13 +14,24 @@
         style = "latte";
       };
 
-      lsp.enable = true;
+      lsp = {
+        enable = true;
+        formatOnSave = true;
+        lspSignature.enable = true;
+      };
 
       languages = {
         nix.enable = true;
-        rust.enable = true;
+        rust = {
+          enable = true;
+          extensions.crates-nvim.enable = true;
+        };
         python.enable = true;
         bash.enable = true;
+        markdown.enable = true;
+        ts.enable = true;
+        html.enable = true;
+        css.enable = true;
       };
 
       treesitter.enable = true;
@@ -28,6 +39,25 @@
       telescope.enable = true;
 
       autocomplete.nvim-cmp.enable = true;
+
+      filetree.neo-tree.enable = true;
+
+      visuals = {
+        nvim-web-devicons.enable = true;
+        indent-blankline.enable = true;
+        nvim-cursorline.enable = true;
+      };
+
+      git = {
+        enable = true;
+        gitsigns.enable = true;
+      };
+
+      autopairs.nvim-autopairs.enable = true;
+
+      comments.comment-nvim.enable = true;
+
+      binds.whichKey.enable = true;
 
       options = {
         number = true;
@@ -37,6 +67,12 @@
         expandtab = true;
         clipboard = "unnamedplus";
         mouse = "a";
+        ignorecase = true;
+        smartcase = true;
+        termguicolors = true;
+        signcolumn = "yes";
+        splitright = true;
+        splitbelow = true;
       };
     };
   };
